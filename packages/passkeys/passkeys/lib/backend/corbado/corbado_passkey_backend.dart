1ada796520d833a51f9696dc0f9224003fa60c5c
Victor Dzhagatspanyan <ge73pam@mytum.de>
2023-07-10 15:11:50 +0200

    update ios header and frontend api url

--- packages/passkeys/passkeys/lib/backend/corbado/corbado_passkey_backend.dart
+++ packages/passkeys/passkeys/lib/backend/corbado/corbado_passkey_backend.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'dart:io' show Platform;
 
 import 'package:flutter/foundation.dart';
 import 'package:passkeys/authenticator/passkey_authenticator.dart';
@@ -12,24 +13,30 @@ import 'package:passkeys/backend/types/exceptions.dart';
 import 'package:passkeys/backend/types/registration.dart';
 
 ///
-class CorbadoPasskeyBackend
-    extends PasskeyBackend<CorbadoTokens> {
+class CorbadoPasskeyBackend extends PasskeyBackend<CorbadoTokens> {
   final PasskeyAuthenticator _authenticator;
 
   ///
   CorbadoPasskeyBackend(this._projectID)
-      : _client = ApiClient(basePath: 'https://$_projectID.auth.corbado.com'),
+      : _client =
+            ApiClient(basePath: 'https://$_projectID.frontendapi.corbado.io'),
         _authenticator = PasskeyAuthenticator() {
     _client.addDefaultHeader('X-Corbado-Project-ID', _projectID);
     debugPrint('CorbadoPasskeyBackend: $_projectID');
-    _authenticator.getFacetID().then(
-          (value) => {
-            debugPrint('setting Origin of API requests to $value'),
-            // TODO: so far we have to uncomment this on iOS devices (we could of course add a platform condition around it)
-            // question is though, if we should also include this header in iOS requests (probably yes) and what we have to do to fix it
-            _client.addDefaultHeader('Origin', value),
-          },
-        );
+
+    if (Platform.isAndroid) {
+      _authenticator.getFacetID().then(
+            (value) => {
+              debugPrint('setting Origin of API requests to $value'),
+              _client.addDefaultHeader('Origin', value),
+            },
+          );
+    } else if (Platform.isIOS) {
+      _client.addDefaultHeader(
+        'Origin',
+        'https://$_projectID.frontendapi.corbado.io',
+      );
+    }
   }
 
   final String _projectID;
@@ -43,13 +50,14 @@ class CorbadoPasskeyBackend
       );
 
       if (result == null) {
-        throw UnexpectedBackendException('passKeyRegisterStart', 'result was null');
+        throw UnexpectedBackendException(
+            'passKeyRegisterStart', 'result was null');
       }
 
       final json = jsonDecode(result.data.challenge) as Map<String, dynamic>;
       final typed = CorbadoRegisterChallenge.fromJson(json);
       return typed.toRegisterInitResponse();
-    } on ApiException catch(e) {
+    } on ApiException catch (e) {
       // TODO: better typed errors from backend
       throw UnexpectedBackendException('passKeyRegisterStart', e.message ?? '');
     }
@@ -68,12 +76,14 @@ class CorbadoPasskeyBackend
       );
 
       if (result == null) {
-        throw UnexpectedBackendException('passKeyRegisterFinish', 'result was null');
+        throw UnexpectedBackendException(
+            'passKeyRegisterFinish', 'result was null');
       }
 
       return CorbadoTokens.fromPassKeyRegisterFinishRsp(result);
-    } on ApiException catch(e) {
-      throw UnexpectedBackendException('passKeyRegisterFinish', e.message ?? '');
+    } on ApiException catch (e) {
+      throw UnexpectedBackendException(
+          'passKeyRegisterFinish', e.message ?? '');
     }
   }
 
@@ -103,7 +113,6 @@ class CorbadoPasskeyBackend
       PassKeyFinishReq(signedChallenge: signedChallenge),
     );
 
-    return CorbadoTokens.fromPassKeyLoginFinishRsp(
-        response!);
+    return CorbadoTokens.fromPassKeyLoginFinishRsp(response!);
   }
 }
