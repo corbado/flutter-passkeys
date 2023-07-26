e083e419361623fbf373b7abe3e1f222256b151b
Nicolai Stein <nicolai_stein@outlook.de>
2023-07-26 09:56:26 +0200

    passkeys_android

--- packages/passkeys/passkeys_android/pigeons/messages.dart
+++ packages/passkeys/passkeys_android/pigeons/messages.dart
@@ -9,44 +9,86 @@ import 'package:pigeon/pigeon.dart';
     ),
   ),
 )
+
+/// Represents a relying party
 class RelyingParty {
+  /// Constructor
   const RelyingParty(this.name, this.id);
 
+  /// Name of the relying party
   final String name;
+
+  /// ID of the relying party
   final String id;
 }
 
+/// Represents a public key credential parameter
 class PubKeyCredParam {
+  /// Constructor
   const PubKeyCredParam(this.type, this.alg);
+
+  /// The type
   final String type;
+
+  /// The algorithm
   final int alg;
 }
 
+/// Represents a user
 class User {
+  /// Constructor
   const User(this.displayName, this.name, this.id, {this.icon});
+
+  /// The display name
   final String displayName;
+
+  /// The name
   final String name;
+
+  /// The ID
   final String id;
+
+  /// The icon
   final String? icon;
 }
 
+/// Represents an allowed credential
 class AllowCredential {
+  /// Constructor
   const AllowCredential(this.type, this.id, this.transports);
+
+  /// The type
   final String type;
+
+  /// The ID
   final String id;
+
+  /// The transports
   final List<String?> transports;
 }
 
+/// Represents an authenticator selection
 class AuthenticatorSelection {
+  /// Constructor
   const AuthenticatorSelection(this.authenticatorAttachment,
       this.requireResidentKey, this.residentKey, this.userVerification);
+
+  /// The authenticator attachment
   final String authenticatorAttachment;
+
+  /// Whether a resident key is required
   final bool requireResidentKey;
+
+  /// The resident key
   final String residentKey;
+
+  /// The user verification
   final String userVerification;
 }
 
+/// Represents a register response
 class RegisterResponse {
+  /// Constructor
   const RegisterResponse({
     required this.id,
     required this.rawId,
@@ -54,13 +96,22 @@ class RegisterResponse {
     required this.attestationObject,
   });
 
+  /// The ID
   final String id;
+
+  /// The raw ID
   final String rawId;
+
+  /// The client data JSON
   final String clientDataJSON;
+
+  /// The attestation object
   final String attestationObject;
 }
 
+/// Represents an authenticate response
 class AuthenticateResponse {
+  /// Constructor
   const AuthenticateResponse({
     required this.id,
     required this.rawId,
@@ -69,10 +120,19 @@ class AuthenticateResponse {
     required this.signature,
   });
 
+  /// The ID
   final String id;
+
+  /// The raw ID
   final String rawId;
+
+  /// The client data JSON
   final String clientDataJSON;
+
+  /// The authenticator data
   final String authenticatorData;
+
+  /// The signature
   final String signature;
 }
 
