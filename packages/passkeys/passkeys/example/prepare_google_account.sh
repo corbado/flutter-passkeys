00e0431a9e58fb68c3b421291fb889170e9eac4a
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-01-20 17:47:27 +0100

    sets up google account for android

--- packages/passkeys/passkeys/example/prepare_google_account.sh
+++ packages/passkeys/passkeys/example/prepare_google_account.sh
@@ -63,7 +63,7 @@ sleep 20
 adb shell input tap 400 600
 sleep 20
 
-flutter build apk --debug
+cd packages/passkeys/passkeys/example && flutter build apk --debug
 
-adb install -r build/app/outputs/flutter-apk/app-debug.apk
-node tests/main.js android
+cd packages/passkeys/passkeys/example && adb install -r build/app/outputs/flutter-apk/app-debug.apk
+cd packages/passkeys/passkeys/example && node tests/main.js android
