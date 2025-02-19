a7260c386e03c9167865f4a9b9cc267ea1ff6503
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-02-19 18:43:25 +0100

    fixes custom domain

--- packages/corbado_auth/example/lib/main.dart
+++ packages/corbado_auth/example/lib/main.dart
@@ -23,6 +23,11 @@ const String envProjectId = String.fromEnvironment(
   defaultValue: DEFAULT_VALUE,
 );
 
+const String envCustomDomain = String.fromEnvironment(
+  'CORBADO_CUSTOM_DOMAIN',
+  defaultValue: DEFAULT_VALUE,
+);
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
@@ -36,7 +41,9 @@ void main() async {
   final projectId =
       envProjectId == 'none' ? calculateProjectID() : envProjectId;
 
-  final customDomain = 'https://$projectId.frontendapi.cloud.corbado.io';
+  final customDomain = envCustomDomain == 'none'
+      ? 'https://$projectId.frontendapi.cloud.corbado.io'
+      : envCustomDomain;
 
   final corbadoAuth = CorbadoAuth();
   await corbadoAuth.init(projectId: projectId, customDomain: customDomain);
