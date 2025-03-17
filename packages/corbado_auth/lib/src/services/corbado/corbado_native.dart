1f1dd0f2d7ffaf65fb088bc13a59c916ac3aeb6f
Dopeamin <mohamed.amine.hamdouni@corbado.com>
2025-03-17 14:47:32 +0100

    adds corbado flags in case of debug and on iOS

--- packages/corbado_auth/lib/src/services/corbado/corbado_native.dart
+++ packages/corbado_auth/lib/src/services/corbado/corbado_native.dart
@@ -4,6 +4,7 @@ import 'package:corbado_auth/src/services/corbado/corbado.dart';
 import 'package:corbado_auth/src/services/storage/storage.dart';
 import 'package:corbado_auth/src/services/storage/storage_native.dart';
 import 'package:corbado_frontend_api_client/corbado_frontend_api_client.dart';
+import 'package:flutter/foundation.dart';
 import 'package:passkeys/authenticator.dart';
 import 'package:ua_client_hints/ua_client_hints.dart';
 
@@ -19,12 +20,19 @@ Future<CorbadoService> createClient(
 
   final uaData = await userAgentData();
 
+  // in case of iOS we want to use the apple dev mode
+  // when we are in debug mode
+  final isAppleDevMode = kDebugMode &&
+          Platform.isIOS;
+
   final apiClient = CorbadoFrontendApiClient(
-      basePathOverride: basePath,
-      sdkVersion: '3.2.0',
-      languageVersion: Platform.version);
+    basePathOverride: basePath,
+    sdkVersion: '3.2.0',
+    languageVersion: Platform.version,
+  );
   apiClient.dio.options.headers.addAll({
     'X-Corbado-ProjectID': projectId,
+    'X-Corbado-Flags': isAppleDevMode ? 'apple_dev_mode' : '',
     'User-Agent': _buildUserAgent(uaData),
   });
 
