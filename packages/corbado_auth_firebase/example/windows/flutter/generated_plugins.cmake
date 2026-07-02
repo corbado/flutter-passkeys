96ddf26deee4b5a91f0b0c54b3219a50a76b0d04
kopy <585539+kopy@users.noreply.github.com>
2026-07-02 16:13:46 +0000

    [create-pull-request] automated change

--- packages/corbado_auth_firebase/example/windows/flutter/generated_plugins.cmake
+++ packages/corbado_auth_firebase/example/windows/flutter/generated_plugins.cmake
@@ -6,9 +6,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_auth
   firebase_core
   flutter_secure_storage_windows
+  passkeys_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  jni
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
