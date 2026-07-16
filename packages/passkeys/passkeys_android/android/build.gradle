5c1b87f19fd69150ab84bc47e2641c50f456531c
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2026-07-16 21:38:01 +0200

    chore(passkeys_android): set jvmTarget via the Property API
    Use jvmTarget.set(...) instead of assignment so it works across Gradle
    versions in the Groovy build script, not only where lazy property
    assignment is supported.

--- packages/passkeys/passkeys_android/android/build.gradle
+++ packages/passkeys/passkeys_android/android/build.gradle
@@ -44,7 +44,7 @@ android {
 
 kotlin {
     compilerOptions {
-        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
